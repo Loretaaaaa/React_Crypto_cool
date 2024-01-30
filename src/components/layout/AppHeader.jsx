4dec30767a21887a29667f95d9086d324d5b77eb
Loreta <badalyan.loreta@gmail.com>
2024-01-30 17:12:50 +0400

    CoinInfoModal fully!

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,7 +1,8 @@
-import { Layout, Select, Space, Button, Modal } from 'antd';
+import { Layout, Select, Space, Button, Modal, Drawer} from 'antd';
 import { useCrypto } from '../../context/crypto-contex';
 import { useState, useEffect } from 'react';
 import CoinInfoModal from '../CoinInfoModal';
+import AddAssetForm from '../AddAssetForm';
 
 const headerStyle = {
   width: '100%',
@@ -18,6 +19,7 @@ export default function AppHeader() {
   const [select, setSelect] = useState(false);
   const [coin, setCoin] = useState(null);
   const [modal, setModal] = useState(false);
+  const [drawer, setDrawer] = useState(false);
   const { crypto } = useCrypto();
 
   useEffect (() => {
@@ -63,7 +65,7 @@ export default function AppHeader() {
       )}
     />
 
-    <Button type="primary">Add Asset</Button>
+    <Button type="primary" onClick={() => setDrawer(true)}>Add Asset</Button>
 
     <Modal 
       open={modal} 
@@ -72,5 +74,14 @@ export default function AppHeader() {
     >
       <CoinInfoModal coin={coin} />
     </Modal>
+
+    <Drawer  
+      width={'40%'} 
+      title="Add Asset" 
+      onClose={() => setDrawer(false)} 
+      open={drawer}
+    >
+        <AddAssetForm />
+      </Drawer>
   </Layout.Header>)
 }
